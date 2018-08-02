d2b9a95021b2093abaa4788a6838239b12af1042
Rizwaan <rizraak@hotmail.com>
2018-08-02 16:49:49 -0400

    WORKING(prototype)

--- index.js
+++ index.js
@@ -1,13 +1,68 @@
-module.exports = generateRandomString
 
-const adjectives = require('adjectives')
-const animals = require('animals')
-const randomNumber = require('random-number-in-range')
+module.exports = (locationXLSXtoMYSQL,optionsXLSXtoMYSQL,waitT) => {
+var xlsx = require('node-xlsx');
+var fs = require('fs');
+var obj = xlsx.parse(locationXLSXtoMYSQL); // parses a file
+resulrt = ' ';
 
-function generateRandomString () {
-  return [randomAdjective(), animals(), randomNumber(10, 99)].join('-')
+for(var i = 0; i < obj.length; i++)
+{
+
+    (function (i) {
+        setTimeout(function () {
+    delete rows;
+    delete sheet;
+    rows = [];
+    sheet = obj[i];
+
+    for(var j = 0; j < sheet['data'].length; j++)
+    {
+
+            while (sheet['data'][j].length < sheet['data'][0].length) {
+                sheet['data'][j].push('');
+            }
+            while (sheet['data'][j].length > sheet['data'][0].length) {
+                sheet['data'][j].pop();
+            }
+            rows.push(sheet['data'][j]);
+
+    }
+
+ return csvPARxlsxmysql(optionsXLSXtoMYSQL.csv.delimiter,optionsXLSXtoMYSQL,i,obj.length);
+
+
+}, waitT*i);
+})(i); 
+
+
 }
 
-function randomAdjective () {
-  return adjectives[randomNumber(0, adjectives.length - 1)]
+}
+function csvPARxlsxmysql(delim,options,q,objL){
+    writeStr = "";
+    for(var i = 0; i < rows.length; i++)
+    {   
+        writeStr += rows[i].join(delim) + "\n";
+    }
+    var data = writeStr;
+    
+
+    options["table"] = sheet['name'];
+    return upload(data,options,q,objL);
+}
+
+function upload(data,options,i,objL){
+
+    var cm = require('csv-mysql');
+    cm.import(options, data, function(err, txt){
+        if( err ){
+            resulrt += sheet['name']+":  " + err+": "+txt+'\n\n';
+        }
+        else{
+            resulrt += sheet['name']+":  " + 'Import completed\n\n';
+        }
+    })
+    if (i == objL - 1){
+        return resulrt;
+    }
 }
--- test/index.js
+++ test/index.js
@@ -1,11 +1,1 @@
-const test = require('tap').test
 
-const generateRandomString = require('../index')
-
-test('smoke test', function (t) {
-  const string = generateRandomString()
-  t.is(typeof string, 'string')
-  t.ok(string.length > 0)
-  t.ok(/-\d{2}$/.test(string))
-  t.end()
-})
